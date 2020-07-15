103855f6bd2ba9b52edf9ee631cf8ef4c3f5bb32
AtanasDimulski <43001169+AtanasDimulski@users.noreply.github.com>
2020-07-15 16:09:57 +0200

    changes

---
+++ Assets/Scripts/SaveObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveObject
+{
+    public int Rows, Cols;
+    public List<SavedTile> SavedTiles = new List<SavedTile>();
+    public float Budget;
+    public float TotalCost;
+    public List<Results> ListOfResults = new List<Results>();
+
+    public SaveObject(){
+        this.Rows = Map.m._rows;
+        this.Cols = Map.m._cols;
+        this.Budget = Map.m.budget;
+        this.TotalCost = Map.m.totalCost;
+        this.ListOfResults = Map.m.listOfResults;
+
+        foreach (var t in Map.m.currentTiles)
+        {
+            SavedTile st = new SavedTile();
+            st.tileID = t.tileID;
+            st.tilePosition = t.tilePosition;
+            st.currentTileType = t.tileType;
+            st.isOuterWall = t.isOuterWall;
+            this.SavedTiles.Add(st);
+        }
+    }
+}
